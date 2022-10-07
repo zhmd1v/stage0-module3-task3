58c2f64d2ffe72bbdc487904d25852a62e1f5e8c
zhmd1v <zhumadelov.kostanay@gmail.com>
2022-10-07 14:03:51 +0600

    Initializtion and declaration

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -3,5 +3,12 @@ package lang.print.gaps.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,5 +3,29 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        int linkToFirst, linkToSecond, linkToThird;
+        linkToFirst = first;
+        linkToSecond = second;
+        linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
